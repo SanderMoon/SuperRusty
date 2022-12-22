fb475fbfcf41b3c9e75de4039532950c7c0735c5
djleotechwizard <l.cornelissen@student.ru.nl>
2022-12-22 18:26:59 +0100

    Added visualisation functions

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use chess_game::ChessGame;
 fn main() {
     let chess : ChessGame = initialize_chess_board();
     println!("{:#?}", &chess);
+    println!("{}", chess.visualize())
 }
 
 fn initialize_chess_board() -> ChessGame{
